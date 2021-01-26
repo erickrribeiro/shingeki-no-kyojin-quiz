861e31f691ea99abb08cb15ebc4fa31ad8dac489
Erick Ribeiro <erick.ribeiro.17@hotmail.com>
2021-01-25 22:56:40 -0400

    Pages with styled components
    Signed-off-by: Erick Ribeiro <erick.ribeiro.17@hotmail.com>

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import db from '../db.json'
+import styled from 'styled-components'
+import { Widget } from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+          <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+          
+          <p> {db.description}</p>
+          </Widget.Content>           
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/erickrribeiro"/>
+    </QuizBackground>      
+  )
+}
