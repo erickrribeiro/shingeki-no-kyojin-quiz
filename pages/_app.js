d9a3a691885730aaa9040466210140ff971a9cc7
Erick Ribeiro <erick.ribeiro.17@hotmail.com>
2021-02-05 22:20:47 -0400

    Was configured Adsense
    Signed-off-by: Erick Ribeiro <erick.ribeiro.17@hotmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import Head from "next/head";
-import db from "../db.json";
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,6 +31,7 @@ const GlobalStyle = createGlobalStyle`
 const { theme } = db;
 
 // eslint-disable-next-line react/react-in-jsx-scope
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -38,6 +40,11 @@ export default function App({ Component, pageProps }) {
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
+        />
+        <script
+          data-ad-client="ca-pub-9240799799016979"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         />
       </Head>
       <ThemeProvider theme={theme}>
